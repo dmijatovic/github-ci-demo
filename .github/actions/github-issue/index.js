300ef5efcdf7d63afd55ccd87e325f73f8e40a1e
Dusan Mijatovic (xps2018) <d.mijatovic@dv4all.nl>
2021-09-26 14:54:31 +0200

    fix issue action async fn

--- .github/actions/github-issue/index.js
+++ .github/actions/github-issue/index.js
@@ -8,18 +8,28 @@ try {
   const body = core.getInput("body");
   const assignTo = core.getInput("assign-to");
 
-  // use to communicate to Github (instead of api with fetch/axios)
   const octokit = new GitHub(token);
-  // create new issue
-  const resp = octokit.issues.create({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    title,
-    body,
-    assignees: assignTo ? assignTo.split(",") : undefined,
-  });
-  // provide issue object as stringified json
-  core.setOutput("issue", JSON.stringify(response.data, null, 2));
+
+  core.notice("creating issue");
+  // async - promise based request
+  octokit.issues
+    .create({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      title,
+      body,
+      assignees: assignTo ? assignTo.split(",") : undefined,
+    })
+    .then((resp) => {
+      core.notice("issue created");
+      // provide issue object as stringified json
+      core.setOutput("issue", JSON.stringify(response.data, null, 2));
+    })
+    .catch((e) => {
+      core.warning("issue not created!");
+      // notify github action about the error
+      core.setFailed(`Failed to create issue: ${e.message}`);
+    });
 } catch (e) {
   // notify github action about the error
   core.setFailed(`Github-issue action failed: ${e.message}`);
