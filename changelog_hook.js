082509c203d6c5e9c5ac862ccbab5aaad5f156bc
Dusan Mijatovic (PC2020) <d.mijatovic@dv4all.nl>
2022-03-07 20:13:38 +0100

    feat: use only tag hook to tag alpha

--- changelog_hook.js
+++ changelog_hook.js
@@ -1,21 +1,12 @@
-// // Next version e.g. 1.12.3
-// export function preVersionGeneration(version: string): string {
+// Next version e.g. 1.12.3
+// exports.preVersionGeneration = (version) => {
 //   console.log("preVersionGeneration...version...", version)
 //   return version
 // }
 
-// // Next tag e.g. v1.12.3
-// export function preTagGeneration(tag: string): string {
-//   console.log("preTagGeneration...tag...", tag)
-//   return `${tag}-alpha`
-// }
-
-exports.preVersionGeneration = (version) => {
-  console.log("preVersionGeneration...version...", version)
-  return version
-}
-
+// Next tag e.g. v1.12.3
 exports.preTagGeneration = (tag) => {
-  console.log("preTagGeneration...tag...", tag)
+  // console.log("preTagGeneration...tag...", tag)
+  // add alpha to all versions now
   return `${tag}-alpha`
 }
